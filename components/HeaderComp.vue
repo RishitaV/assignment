a642ca9e2e9d50c747f2eea9bae12d734b4871cb
RishitaV <75077060+RishitaV@users.noreply.github.com>
2023-08-14 17:37:14 +0530

    basic

---
+++ components/HeaderComp.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="nav">
+    <div class="options">Logo</div>
+    <div class="options" @click="logout()">Logout</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComp",
+  methods: {
+    logout() {
+        localStorage.clear();
+        this.$router.push({name: 'LoginPage'});
+    }
+  }
+};
+</script>
+
+<style scoped>
+.nav {
+  background-color: #333;
+  display: flex;
+  justify-content: space-between;
+  color: white;
+}
+.options {
+    cursor: pointer;
+}
+</style>
